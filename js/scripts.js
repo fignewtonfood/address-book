7afdcec7e7c1a9e2b6dbd8a8ff7e8bca40a67c75
Tim White & Ike Mattice <student@epicodus.com>
2015-09-15 13:48:10 -0700

    refactored out html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,64 @@
+function Contact(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.addresses = [];
+}
+
+Contact.prototype.fullName = function() {
+    return this.firstName + ' ' + this.lastName;
+}
+
+function Address(street, city, state, addressType) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.addressType = addressType;
+}
+
+Address.prototype.fullAddress = function() {
+    return this.addressType + ": " + this.street + ", " + this.city + ", " + this.state;
+}
+
+function resetAddress() {
+    $("input.new-street").last().val("");
+    $("input.new-city").last().val("");
+    $("input.new-state").last().val("");
+    $("input.new-addressType").last().val("Home");
+}
+
+function resetFields() {
+    $("input").val("");
+}
+
+
+
+//jQuery
 $(document).ready(function(){
     $("#add-address").click(function(){
-        $("#new-addresses").append( '<div class="new-address">' +
-                                        '<div class="form-group">' +
-                                            '<label for="new-street">Street</label>' +
-                                            '<input type="text" class="form-control new-street">' +
-                                        '</div>' +
-                                        '<div class="form-group">' +
-                                            '<label for="new-city">City</label>' +
-                                            '<input type="text" class="form-control new-city">' +
-                                        '</div>' +
-                                        '<div class="form-group">' +
-                                            '<label for="new-state">State</label>' +
-                                            '<input type="text" class="form-control new-state">' +
-                                        '</div>');
+        $(".new-address").clone().last().appendTo('#new-addresses');
+        resetAddress();
     });
+
     $("form#new-contact").submit(function(event){
         event.preventDefault();
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName = $("input#new-last-name").val();
-        var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+        var newContact = new Contact (inputtedFirstName, inputtedLastName);
 
         $(".new-address").each(function(){
             var inputtedStreet = $(this).find("input.new-street").val();
             var inputtedCity = $(this).find("input.new-city").val();
             var inputtedState = $(this).find("input.new-state").val();
+            var inputtedAddressType = $(this).find(".new-addressType").val();
 
-            var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
-
+            var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedAddressType);
             newContact.addresses.push(newAddress);
+
+            resetFields();
         });
 
         $("ul#contacts").append("<li><span class='contact'>" +
-          newContact.firstName + "</span></li>");
+          newContact.fullName() + "</span></li>");
 
         $(".contact").last().click(function(){
             $("#show-contact").show();
@@ -40,15 +67,11 @@ $(document).ready(function(){
             $(".last-name").text(newContact.lastName);
             $("ul#addresses").text("");
             newContact.addresses.forEach(function(address){
-                $("ul#addresses").append("<li>" + address.street + ", " +
-                  address.city + ", " + address.state + "</li>");
+                $("ul#addresses").append("<li>" + address.addressType + ": " + address.street + ", " + address.city + ", " + address.state + "</li>");
+
             });
         });
 
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
-        $("input.new-street").val("");
-        $("input.new-city").val("");
-        $("input.new-state").val("");
+        $(".new-address").not(':first').remove();
     });
 });
